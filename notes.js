2f72fc93128c072fed6c84ee9c2d78e6be3adcc3
Davee Sok <daveesok@gmail.com>
2020-08-10 14:44:04 -0700

    app now functional. will display correct messages

--- notes.js
+++ notes.js
@@ -3,10 +3,9 @@
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
 
+
 const convertedInput = new Input();
-
 const note = new Notes(convertedInput);
-
 const output = note.execute();
 
-console.log('output: ', output);
+console.log(output);
