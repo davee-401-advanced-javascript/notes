a707d940f2faccf832b1509ce3665fbd887ccb59
Davee Sok <daveesok@gmail.com>
2020-08-14 16:57:03 -0700

    refactored getAction and getpayload in input

--- notes.js
+++ notes.js
@@ -5,7 +5,9 @@ const Notes = require('./lib/notes.js');
 
 const convertedInput = new Input();
 // console.log('testconvert:', convertedInput.valid());
+
 const note = new Notes(convertedInput);
 const output = note.execute();
 
 console.log(output);
+ 
