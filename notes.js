ad01dc38556ea832950a1531e6f6c3d9ebf44b53
Davee Sok <daveesok@gmail.com>
2020-08-14 16:04:58 -0700

    refactored get action

--- notes.js
+++ notes.js
@@ -4,7 +4,7 @@ const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
 
 const convertedInput = new Input();
-
+// console.log('testconvert:', convertedInput.valid());
 const note = new Notes(convertedInput);
 const output = note.execute();
 
