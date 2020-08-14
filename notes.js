11ff3c424fc14c2e2b8ab8641066c0e86f1eb617
Davee Sok <daveesok@gmail.com>
2020-08-13 18:02:30 -0700

    refactored notes with classes

--- notes.js
+++ notes.js
@@ -3,7 +3,6 @@
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
 
-
 const convertedInput = new Input();
 const note = new Notes(convertedInput);
 const output = note.execute();
