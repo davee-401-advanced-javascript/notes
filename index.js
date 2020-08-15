f4a42136c188d7db024be4078b81c0d3f5a4f7cd
Davee Sok <daveesok@gmail.com>
2020-08-14 18:36:21 -0700

    refactored input js again

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
 
 const convertedInput = new Input();
-// console.log('testconvert:', convertedInput.valid());
+
+
 
 const note = new Notes(convertedInput);
 const output = note.execute();
-
 console.log(output);
- 
