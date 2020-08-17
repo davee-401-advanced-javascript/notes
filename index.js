785210b3d38b91bd4530c4b3313c9752f4db9ba6
Davee Sok <daveesok@gmail.com>
2020-08-16 20:57:42 -0700

    constructor can now account for category

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 'use strict';
 
-const mongoose = require('mongoose');
-
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
 
 const input = new Input();
+// console.log('input: ', input);
 
 if (input.valid()) {
   const note = new Notes(input);
   note.execute();
-} 
+} else {
+  process.exit(9);
+}
 
