8cb41e819bb490c89feb5a501a75d2d9d66cef7b
Davee Sok <daveesok@gmail.com>
2020-08-21 19:12:47 -0700

    refactored input js to return undefined instead of true in payload and category properties

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
 
 const input = new Input();
-// console.log('input: ', input);
+console.log('input: ', input);
 
 if (input.valid()) {
   const note = new Notes(input);
